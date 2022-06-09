ebdadabdc7e559ea35f8691cefebf66fa7e4c1c6
apopheny <cedouglass@gmail.com>
2022-06-09 18:45:22 -0400

    some easy excercises

---
+++ easy2_02.rb
@@ -0,0 +1,25 @@
+=begin
+Zipper
+
+The Array#zip method takes two arrays, and combines them into a single array in which each element is a two-element array where the first element is a value from one array, and the second element is a value from the second array, in order. For example:
+
+[1, 2, 3].zip([4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]
+
+Write your own version of zip that does the same type of operation. It should take two Arrays as arguments, and return a new Array (the original Arrays should not be changed). Do not use the built-in Array#zip method. You may assume that both input arrays have the same number of elements.
+=end
+
+def zip(arr1, arr2)
+  result = Array.new
+  end_index = [arr1.size, arr2.size].max
+
+  counter = 0
+  loop do
+    result << [arr1[counter], arr2[counter]]
+    counter += 1
+    break if counter == end_index
+  end
+
+  result
+end
+
+p zip([1, 2, 3], [4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]
